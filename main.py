ec158173f8a93b30c71ac5077a9c8c4aa92ba71a
21819 <tomorasd@foxmail.com>
2024-06-04 04:59:33 +0800

    右键样式美化

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QSlider, QWidget, QVBoxLayout, QWidgetAction
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QSystemTrayIcon, QMenu, QSlider, QLabel, QHBoxLayout, QWidgetAction, \
+    QAction
 from PyQt5.QtGui import QIcon, QPainter, QColor
+from PyQt5.QtCore import Qt
 
 """
 文件名: main.py
-版本: 1.0
+版本: 1.1
 用途: 使用PyQt5创建一个系统托盘应用程序，通过托盘图标上的滑块来控制屏幕亮度。
 作者: 好吃的秋梨膏
-更新日期: 2024-6-1
+更新日期: 2024-6-4
 """
+
+
 class BrightnessControl(QWidget):
     def __init__(self):
         super().__init__()
@@ -46,13 +49,31 @@ class TrayIcon(QSystemTrayIcon):
         self.setIcon(QIcon("bg.png"))
 
         self.menu = QMenu()
+
+        # 创建水平布局
+        slider_layout = QHBoxLayout()
+
+        # 创建滑动条
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setRange(0, 100)
         self.slider.setValue(0)
         self.slider.valueChanged.connect(self.change_brightness)
 
+        # 创建标签来显示百分比
+        self.label = QLabel("0%")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setFixedWidth(40)
+
+        # 将滑动条和标签添加到布局
+        slider_layout.addWidget(self.slider)
+        slider_layout.addWidget(self.label)
+
+        # 创建一个小部件用来承载布局
+        slider_widget = QWidget()
+        slider_widget.setLayout(slider_layout)
+
         self.sliderAction = QWidgetAction(self.menu)
-        self.sliderAction.setDefaultWidget(self.slider)
+        self.sliderAction.setDefaultWidget(slider_widget)
         self.menu.addAction(self.sliderAction)
 
         self.quitAction = QAction("退出", self)
@@ -65,9 +86,17 @@ class TrayIcon(QSystemTrayIcon):
         self.brightnessControl = BrightnessControl()
         self.brightnessControl.show()
 
+        # 添加QSS样式表
+        self.loadStyles()
+
+    def loadStyles(self):
+        with open("style.qss", "r") as fh:
+            self.menu.setStyleSheet(fh.read())
+
     def change_brightness(self, value):
         self.brightnessControl.brightness = value
         self.brightnessControl.updateBrightness()
+        self.label.setText(f"{value}%")
 
     def exit(self):
         QApplication.quit()
